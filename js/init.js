d7c1497e3014a99dd9d826fdf3c59587569ce5b9
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2021-10-26 14:26:27 +0200

    rajout de init js pour récupérer fetch

--- js/init.js
+++ js/init.js
@@ -5,4 +5,4 @@ const myFetch = async () =>{
     .then(function(resp){return resp.json();})
     .then(function(data){return data})
     .catch(function(error) {console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);});
-  };
+};
