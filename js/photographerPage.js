d59c92e9552104798547d9b6512c5596782c063c
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2021-10-26 14:37:43 +0200

    déclaration de variables pour récupérer les photographes targeted

--- js/photographerPage.js
+++ js/photographerPage.js
@@ -6,3 +6,9 @@ const main = document.getElementById('main-section');
 mediaArray= [];
 mediaImage= [];
 mediaVideo= [];
+
+//récupérer targeted photographer lors du click précédent (de index.html)
+let myUrl = window.location.href;
+let url_string = (window.location.href);
+let url = new URL(url_string);
+let targetId = url.searchParams.get("id");
