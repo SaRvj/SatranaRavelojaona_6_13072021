f49d44b51a03cf99d76651538b260d0b4fe26c82
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2021-10-27 15:10:41 +0200

    création d'éléments sur le DOM

--- js/mediaFactoryMethod.js
+++ js/mediaFactoryMethod.js
@@ -10,5 +10,18 @@ function MediaFactory(thatMedia){
         //récupérer les éléments du DOM
         let portfolio = document.querySelector(".portfolio");
     
+        //création d'élement sur le DOM
+        let media = document.createElement('article');
+        let upMedia = document.createElement('div');
+        let image = document.createElement('img');
+        let downMedia = document.createElement('div');
+        let mediaTitle = document.createElement('p');
+        let mediaDate = document.createElement('p');
+        let mediaPrice = document.createElement('p');
+        let likes = document.createElement('div');
+        let number = document.createElement('div');
+        let heart = document.createElement('i');
+        let thisMediaTags = document.createElement('div');
+        let hidenTitle = document.createElement('div');
     }
 }
