d7eaabc021164264f9ef7211576845d1a83fac55
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2021-10-29 14:31:00 +0200

    récupérer les articles dans media

--- js/sortingContent.js
+++ js/sortingContent.js
@@ -54,4 +54,30 @@ function sortingMediaTags (){
     //récupérer les class des tags
     const allTags = document.querySelectorAll(".tagportrait,.art,.fashion,.architecture,.travel,.sports,.animals,.events");
     const allTagsTag = document.querySelectorAll(".allTags");
+
+    //écoute des click sur alltags
+    for (let i = 0; i < allTags.length ; i++){
+        allTags[i].addEventListener("click", function (event){
+
+            //récupérer les className des tags séléctionnés
+            let targetTag = event.target.className;
+
+            //récupérer tous les articles dans Media
+            let mediaArticles = document.getElementsByTagName("article");
+            Array.from(PhotographerSections).map(element => { //création d'une nouvelle instance d'Array
+                let inTags = element.lastChild.previousSibling.id;
+                
+                //si le tag sélectionné n'est pas présent, la carte du photographe en cours est supprimé
+                if (inTags == targetTag){
+                    element.style.display = "block";
+                }
+
+                //si le tag sélectionné est présent, la carte du photographe en cours est affiché
+                else {
+                    element.style.display = "none";
+                }        
+                return element;
+            });      
+        });
+    }
 }
