77658288b41687ecb2bbc382f89686600aa5b66c
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2021-10-29 09:43:53 +0200

    récupération des tags sur le dom

---
+++ js/sortingContent.js
@@ -0,0 +1,6 @@
+//écoute des tags cliqués et affichage des pages de photographe corresspondant
+function sortingTags (){
+    //récupérer les class des tags
+    const allTags = document.querySelectorAll(".tagportrait,.art,.fashion,.architecture,.travel,.sports,.animals,.events");
+    const allTagsTag = document.querySelectorAll(".allTags");
+}
