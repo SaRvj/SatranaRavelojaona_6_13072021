0cb6ad759658215f0661ac5a11fe6cd98f1f8627
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2021-11-03 16:12:59 +0100

    création fonction putmodals, lunchlightbox

--- js/modalsLightboxes.js
+++ js/modalsLightboxes.js
@@ -27,6 +27,80 @@ function putModals(){
     //au click
     lightBtn.forEach((btn) => btn.addEventListener("click", launchLightBox));
     lightBtnVideo.forEach((btn) => btn.addEventListener("click", launchLightBoxVideo));
-    
+
+    //en appuyant sur entrer sur focus
+    lightBtn.forEach((btn) => btn.addEventListener("keyup", launchLightBox2));  
+    lightBtnVideo.forEach((btn) => btn.addEventListener("keyup", launchLightBoxVideo2));
+  
+    /*fermer la modal et lightboxes*/
+    //au click sur X
+    closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+
+    //en appuyant sur entrer sur focus
+    closeBtn.forEach((btn) => btn.addEventListener("keyup", ckeckKeyClose));
+
+    //en appuyant sur échap
+    document.addEventListener('keydown', function(e) {
+        if(lightBoxIsOpen == true || ModalIsOpen == true){        
+            //fermer la modale et lightbow en appuyant sur échap
+            if (KeyboardEvent.key === 27){
+                closeModal();
+            }
+  
+            //change media en lightbox en pressant la flèche
+  
+            //flèche gauche pour précédent
+            else if(KeyboardEvent.key === 37){
+                let target = document.querySelector(".back");
+                target.click(); 
+            }
+  
+            //flèche droite pour suivant
+            else if (KeyboardEvent.key === 39){
+                let target = document.querySelector(".next");
+                target.click(); 
+            }
+        }
+    });
+
+    /*fonctions*/
+    //lancer la modale formulaire
+    function launchModal(){
+        ModalIsOpen = true;
+        modalbg.style.display = "block";
+    }
+
+    //lancer la lightbox contenant les images par click
+    function launchLightBox(){ 
+        lightBoxIsOpen = true;
+  
+        //afficher ligthbox
+        lightBox.style.display = "block";       
+  
+        //inserer le titre corect des imagesInsert correct Image Title  
+        let mediaTitle = document.getElementById("media-title-ligthbox");
+        let targetedTitle = e.target.id;
+        mediaTitle.innerHTML = targetedTitle;
+          
+        //insérer l'image correcte
+        let selectedMedia = document.querySelector(".media-ligthbox");
+        let targetedImg = e.target.src;
+        let targetedAlt = e.target.alt;
+  
+  
+        //vérifier s'il y a déjà une image
+        if (selectedMedia.hasChildNodes()){}
+        else{
+          //insérer l'image une fois
+          let imageLightbox = document.createElement('img');
+          imageLightbox.id = "image-lightbox";    
+          selectedMedia.appendChild(imageLightbox);
+          imageLightbox.src = targetedImg;
+          imageLightbox.alt = targetedAlt;
+        }
+        
+      }
+  
+  
 
 }
