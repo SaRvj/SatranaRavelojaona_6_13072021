4f951893da7ba114e815534616b83780703ab9b5
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2021-11-11 02:26:50 +0100

    nettoyage

--- js/init.js
+++
@@ -1,8 +0,0 @@
-//récupérer le data json
-const myFetch = async () =>{ 
-
-    return await fetch('https://raw.githubusercontent.com/SaRvj/SatranaRavelojaona_6_13072021/master/FishEyeData.json')
-    .then(function(resp){return resp.json();})
-    .then(function(data){return data})
-    .catch(function(error) {console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);});
-};
